05c93bd6090995e7f7a2818a80b62bf688f66d8b
OrangeScientist1 <orangescientist1@outlook.com>
2022-05-15 23:23:53 -0400

    Added Basic Enemy

--- Frogs2Game/Assets/Scripts/EnemyHitbox.cs
+++ Frogs2Game/Assets/Scripts/EnemyHitbox.cs
@@ -6,12 +6,16 @@ public class EnemyHitbox : MonoBehaviour
 {
     [SerializeField]
     GameObject Enemy;
+    [SerializeField]
+    GameObject ParticleEffect;
+
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Sword"){
             EnemyDeath();
         }    
     }
     public void EnemyDeath(){
+        Instantiate(ParticleEffect, Enemy.transform.position, Quaternion.identity);
         Destroy(Enemy);
     }
 }
